35064a71196e2f85e81a7a3755fb1eef347daa0b
j-beckman <beckman.joshua@gmail.com>
2013-09-10 19:45:48 -0500

    Adding generators

--- routes/api.js
+++ routes/api.js
@@ -26,8 +26,36 @@ var Account = require('../models/account')
     , Subject = require('../models/subject');
 
 module.exports = function (app, io, ensureApiAuth) {
-  app.get('/register', function(req, res) {
-    res.render('register', { title: 'Register', user: req.user, message: req.flash('info'), error: req.flash('error') });
+  app.get('/dummy/subject', function(req, res) {
+    var Chance = require('chance');
+    var chance = new Chance();
+    var newSubject = new Subject();
+    newSubject.name = chance.name();
+    newSubject.description = chance.paragraph();
+    newSubject.email = chance.email();
+    newSubject.link = chance.domain();
+    newSubject.password = chance.word();
+    newSubject.save(function(err,resultSubject){
+      res.writeHead(200, { 'Content-Type': 'application/json' });
+      res.write(JSON.stringify(resultSubject));
+      res.end();
+    })
+  });
+  app.get('/dummy/review', function(req, res) {
+    var Chance = require('chance');
+    var chance = new Chance();
+    Subject.findOne().lean().exec(function(err, subject){
+      var newReview = new Review();
+      newReview.subjectId = subject._id;
+      newReview.content = chance.paragraph();
+      newReview.title = chance.string();
+      newReview.rating = chance.floating({min: 0, max: 10});
+      newReview.save(function(err,resultReview){
+        res.writeHead(200, { 'Content-Type': 'application/json' });
+        res.write(JSON.stringify(resultReview));
+        res.end();
+      })
+    })
   });
   app.get('/api/:key/finches/:id', ensureApiAuth, function(req, res) {
     Subject.findOne({_id:req.params.id}).lean().exec(function(err, subject){
